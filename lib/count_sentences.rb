03331f279e12fb5e791d32c96ded962b2f8ea7c6
vivianne <viviannjoroge91@gmail.com>
2023-02-13 15:00:48 +0300

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
+    self[-1] == '?'
 
   end
 
   def exclamation?
+    self[-1] == '!'
+   
 
   end
 
   def count_sentences
+    self.split(/[.?!]+/).count
 
   end
 end
